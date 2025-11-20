d6c22bd5b94da1b8df5aaa50ae2454c4941b3b37
Dean <dnbr1420@gmail.com>
2025-11-20 14:16:52 +0100

    Colours adjusted and Header/Navbar reworked

--- webapp/src/frontend/lib/header.js
+++ webapp/src/frontend/lib/header.js
@@ -1,66 +1,59 @@
 
 
-
-function create_nav(header) {
+/**
+ * role:menu hinzufügen für nummerierung der Elemete
+ * has popup: ? -> Auch beim Model und auf der create todo seite hinzügen
+ * is expanded: ?
+ * contols anything:
+ * Mehr indikatoren für was gerade passiert
+*/
+function create_nav(header, current_page) {
     const nav = document.createElement('nav');
-
+    
     // Label
     const label = document.createElement('label');
     label.setAttribute('for', 'page-select');
-    label.textContent = 'Choose a page:';
-
-    // Select
-    const select = document.createElement('select');
-    select.name = 'page-select';
-    select.id = 'page-select';
-
-    // Options
-    const options = [
+    label.textContent = 'Navigate to:';
+    
+    nav.id = 'page-select';
+    nav.appendChild(label);
+    
+    
+    // Pages
+    const pages = [
         { value: './index.html', text: 'Current To-Do' },
         { value: './todo_list.html', text: 'To-Do List' },
         { value: './add_todo.html', text: 'Add To-Do' }
     ];
+    
+    
+    pages.forEach(pageData => {
+        const page_link = document.createElement('a');
+        page_link.href = pageData.value;
+        page_link.textContent = pageData.text;
 
-    options.forEach(optionData => {
-        const option = document.createElement('option');
-        option.value = optionData.value;
-        option.textContent = optionData.text;
-        select.appendChild(option);
-    });
-
-    // On option selected
-    select.addEventListener('change', function () {
-        const selectedPage = this.value;
-        if (selectedPage) {
-            window.location.href = selectedPage;
+        // Aktuelle Seite markieren
+        if (pageData.value === current_page) {
+            page_link.setAttribute('aria-current', 'page');
+            page_link.id = 'current-page';
         }
+
+        nav.appendChild(page_link);
     });
 
-    // Append label and select to nav
-    nav.appendChild(label);
-    nav.appendChild(select);
     header.appendChild(nav);
-
-    return select
 }
 
-
-function create_settings_button(header) {
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.textContent = ' Open Display Settings';
-
-    header.appendChild(button);
-}
+    
+    
 
 
-
-export function render_header(current_select_value){
-  // Header
-  const header = document.getElementsByTagName("header")[0];
-
-  const nav = create_nav(header);
-  nav.value = current_select_value;
-
-  create_settings_button(header);
+    export function render_header(current_page){
+        // Header
+        const header = document.getElementsByTagName("header")[0];
+        
+        const nav = create_nav(header, current_page);
+        
+  // TODO
+  //create_settings_button(header);
 }
